e06f30cb7e641e931a2f3ec4598e9eb7b211d60a
Joshua Welch <jwelch@cs.unc.edu>
2018-04-10 17:56:40 -0400

    Added functions for quantile normalization, t-SNE, and plotting.

--- R/Analogizer.R
+++ R/Analogizer.R
@@ -15,6 +15,7 @@ NULL
 #' @slot W Shared gene loading factors
 #' @slot V Dataset-specific gene loading factors
 #' @slot tsne.coords Matrix of 2D coordinates obtained from running t-SNE on H.norm
+#' @slot clusters Joint cluster assignments for cells
 #' @slot parameters List of parameters used in analysis
 #'
 #' @name analogizer
@@ -32,10 +33,11 @@ analogizer <- methods::setClass(
     scale.data = "list",
     var.genes = "vector",
     H = "list",
-    H.norm = "list",
+    H.norm = "matrix",
     W = "matrix",
     V = "list",
     tsne.coords = "matrix",
+    clusters= "factor",
     parameters = "list"
   )
 )
@@ -56,6 +58,7 @@ Analogizer <- function(raw.data) {
 #' @param cex.use point size for plot
 #' @param combine How to combine variable genes across experiments. Either "union" or "intersect"
 #' @param keep.unique Keep genes that occur (i.e., there is a corresponding column in raw.data) only in one dataset?
+#' @param capitalize Capitalize gene names to match homologous genes?
 #' @return analogizer object
 #' @export
 #' @examples
@@ -67,11 +70,12 @@ Analogizer <- function(raw.data) {
 #' analogy = selectGenes(analogy)
 #' }
 
-selectGenes = function(object,alphathresh=0.99,varthresh=0.1,cex.use=0.3,combine="union",keep.unique=F)
+selectGenes = function(object,alphathresh=0.99,varthresh=0.1,cex.use=0.3,combine="union",keep.unique=F,capitalize=F)
 {
   genes.use = c()
   for (i in 1:length(object@raw.data))
   {
+  if (capitalize){rownames(object@raw.data)=toupper(rownames(object@raw.data))}
   trx_per_cell <- colSums(object@raw.data[[i]])
   gene_expr_mean <- rowMeans(object@norm.data[[i]])  # Each gene's mean expression level (across all cells)
   gene_expr_var  <- apply( object@norm.data[[i]], 1, var  )  # Each gene's expression variance (across all cells)
@@ -88,21 +92,24 @@ selectGenes = function(object,alphathresh=0.99,varthresh=0.1,cex.use=0.3,combine
   if (combine=="union")
   {
     genes.use = union(genes.use,genes.new)
-    if (!keep.unique)
-    {
-      genes.use = genes.use[genes.use %in% rownames(object@raw.data)]
-    }
   }
   if (combine=="intersection")
   {
     genes.use = intersect(genes.use,genes.new)
   }
   }
+  if (!keep.unique)
+  {
+    for (i in 1:length(object@raw.data))
+    {
+      genes.use = genes.use[genes.use %in% rownames(object@raw.data[[i]])] 
+    }
+  }
   object@var.genes = genes.use
   return(object)
 }
 
-#' Normalize datasets to column sums
+#' Normalize raw datasets to column sums
 #'
 #' @param object analogizer object. 
 #' @return analogizer object
@@ -137,8 +144,132 @@ normalize = function(object)
 
 scaleNotCenter = function(object)
 {
-  object@scale.data = lapply(object@norm.data[object@var.genes,],function(x){scale(t(x),center=F,scale=T)})
+  object@scale.data = lapply(object@norm.data,function(x){scale(t(x[object@var.genes,]),center=F,scale=T)})
+  for (i in 1:length(object@scale.data))
+  {
+    object@scale.data[[i]][is.na(object@scale.data[[i]])]=0
+  }
   return(object)
+}
+
+#' Quantile normalizes cell factors
+#'
+#' @param object analogizer object. Should run optimizeALS before calling.
+#' @param quantiles Number of quantiles to use for quantile normalization
+#' @param ref_dataset Name of dataset to use as a "reference" for normalization. By default,
+#' the dataset with the largest number of cells is used.
+#' 
+#' @return analogizer object
+#' @export
+#' @examples
+#' \dontrun{
+#' Y = matrix(c(1,2,3,4,5,6,7,8,9,10,11,12),nrow=4,byrow=T)
+#' Z = matrix(c(1,2,3,4,5,6,7,6,5,4,3,2),nrow=4,byrow=T)
+#' analogy = Analogizer(list(Y,Z))
+#' analogy@var.genes = c(1,2,3,4)
+#' analogy = scaleNotCenter(analogy)
+#' }
+quantile_norm = function(object,quantiles=50,ref_dataset=NULL)
+{
+  if (is.null(ref_dataset))
+  {
+    ns = sapply(object@scale.data,nrow)
+    ref_dataset = names(object@scale.data)[which.max(ns)]
+  }
+  
+  Hs_scaled = object@H
+  for (i in 1:ncol(Hs_scaled[[1]]))
+  {
+    for (j in 1:length(Hs_scaled)){
+      Hs_scaled[[j]][,i] = object@H[[j]][,i]/sqrt(sum(object@H[[j]][,i]^2))
+    }
+  }
+  labels = list()
+  for(i in 1:length(Hs_scaled)){
+    knn_k=15
+    knn = get.knn(object@H[[i]],knn_k)
+    labels[[i]] = as.factor(apply(Hs_scaled[[i]],1,which.max))
+    labels[[i]] = as.factor(t(apply(knn$nn.index,1,function(x){which.max(table(labels[[i]][x]))}))[1,])
+  }
+  
+  object@clusters = as.factor(unlist(lapply(labels,as.character)))
+  
+  clusters = labels
+  names(clusters)=names(object@H)
+  dims = ncol(object@H[[ref_dataset]])
+  
+  Hs = object@H
+  num_clusters = dims
+    for (k in 1:length(Hs))
+    {
+      for (i in 1:dims)
+      {
+        for (j in 1:num_clusters)
+        {
+          if (sum(clusters[[ref_dataset]]==j)==0 | sum(clusters[[k]]==j)==0){next}
+          if (sum(clusters[[k]]==j)==1){
+            Hs[[k]][clusters[[k]]==j,i] = mean(Hs[[ref_dataset]][clusters[[ref_dataset]]==j,i])
+            next
+          }
+          q2 = quantile(Hs[[k]][clusters[[k]]==j,i],seq(0,1,by=1/quantiles))
+          q1 = quantile(Hs[[ref_dataset]][clusters[[ref_dataset]]==j,i],seq(0,1,by=1/quantiles))
+          if (sum(q1)==0 | sum(q2)==0 | length(unique(q1)) < 2 | length(unique(q2)) < 2)
+          {
+            new_vals = rep(0,sum(clusters[[k]]==j))
+          }
+          else
+          {
+            warp_func = approxfun(q2,q1)  
+            new_vals = warp_func(Hs[[k]][clusters[[k]]==j,i])
+          }
+          
+          Hs[[k]][clusters[[k]]==j,i] = new_vals
+        }
+      }
+    }
+    object@H.norm = Reduce(rbind,Hs)
+  return(object)
+}
+
+#' Perform t-SNE on the normalized cell factors to generate a 2D embedding for visualization
+#'
+#' @param object analogizer object. Should run quantile_norm before calling.
+#' 
+#' @return analogizer object
+#' @export
+#' @examples
+#' \dontrun{
+#' Y = matrix(c(1,2,3,4,5,6,7,8,9,10,11,12),nrow=4,byrow=T)
+#' Z = matrix(c(1,2,3,4,5,6,7,6,5,4,3,2),nrow=4,byrow=T)
+#' analogy = Analogizer(list(Y,Z))
+#' analogy@var.genes = c(1,2,3,4)
+#' analogy = scaleNotCenter(analogy)
+#' }
+run_tSNE = function(object)
+{
+  object@tsne.coords = Rtsne(object@H.norm,pca=F)$Y
+  return (object)
+}
+
+#' Plot t-SNE coordinates of aligned datasets, colored by dataset and by cluster
+#'
+#' @param object analogizer object. Should call run_tSNE before calling.
+#' @export
+#' @examples
+#' \dontrun{
+#' Y = matrix(c(1,2,3,4,5,6,7,8,9,10,11,12),nrow=4,byrow=T)
+#' Z = matrix(c(1,2,3,4,5,6,7,6,5,4,3,2),nrow=4,byrow=T)
+#' analogy = Analogizer(list(Y,Z))
+#' analogy@var.genes = c(1,2,3,4)
+#' analogy = scaleNotCenter(analogy)
+#' }
+plotByDatasetAndCluster = function(object)
+{
+  tsne_df = data.frame(object@tsne.coords)
+  colnames(tsne_df)=c("tsne1","tsne2")
+  tsne_df$Dataset = unlist(sapply(1:length(object@H),function(x){rep(names(object@H)[x],nrow(object@H[[x]]))}))
+  tsne_df$Cluster = object@clusters
+  print(plot_grid(ggplot(tsne_df,aes(x=tsne1,y=tsne2,color=Dataset))+geom_point(),ggplot(tsne_df,aes(x=tsne1,y=tsne2,color=Cluster))+geom_point()))
 }
 
 #' show method for analogizer
@@ -157,7 +288,7 @@ setMethod(
       "An object of class",
       class(object),
       "\n",
-      length(norm.data),
+      length(object@norm.data),
       "datasets.\n"
     )
     invisible(x = NULL)
@@ -185,7 +316,7 @@ setMethod(
 #' }
 optimizeALS = function(object,k,lambda=5.0,thresh=1e-4,max_iters=100,nrep=20)
 {
-  E = lapply(object@scale.data,t)
+  E = object@scale.data
   N = length(E)
   ns = sapply(E,nrow)
 
