ef27e0416d9512d64a645f321b002f39490fb00a
derpylz <nils.trost@hotmail.de>
2020-09-16 14:04:03 +0200

    make subsetLiger work before normalization and/or clustering

--- R/liger.R
+++ R/liger.R
@@ -4885,13 +4885,14 @@ subsetLiger <- function(object, clusters.use = NULL, cells.use = NULL, remove.mi
   a@H <- lapply(1:length(a@raw.data), function(i) {
     object@H[[nms[i]]][colnames(a@raw.data[[i]]), ]
   })
-  a@clusters <- object@clusters[unlist(lapply(a@H, rownames))]
+  cell.nms <- unname(unlist(lapply(a@raw.data, colnames)))
+  a@clusters <- object@clusters[cell.nms]
   a@clusters <- droplevels(a@clusters)
-  a@tsne.coords <- object@tsne.coords[names(a@clusters), ]
+  a@tsne.coords <- object@tsne.coords[cell.nms, ]
   a@H.norm <- object@H.norm[names(a@clusters), ]
   # Add back additional cell.data
   if (ncol(a@cell.data) < ncol(object@cell.data)) {
-    a@cell.data <- droplevels(data.frame(object@cell.data[names(a@clusters), ]))
+    a@cell.data <- droplevels(data.frame(object@cell.data[cell.nms, ]))
   }
   
   a@W <- object@W
